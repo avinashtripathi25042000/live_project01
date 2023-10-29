af31420e51f327217d3850b3412cd84598a10e3b
avinashtripathi25042000 <132208287+avinashtripathi25042000@users.noreply.github.com>
2023-10-29 13:38:23 +0530

    ssecond

--- src/My_page/Contact.js
+++ src/My_page/Contact.js
@@ -1,10 +1,23 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
-export class Contact extends React.Component{
-    render()
-    {
-        return(
-            <h1><a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://m.facebook.com/p/Jagatguru-Shri-Vallabhacharya-ji-Maharaj-19826964486-100063566028142/&ved=2ahUKEwjQsqG38YOCAxU6zTgGHbGvAgUQFnoECBIQAQ&usg=AOvVaw2rI4wT3O39DB7_vO9uUmQs">facebook</a></h1>
-        );
-    }
+export class Contact extends React.Component
+ 
+{
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://m.facebook.com/p/Jagatguru-Shri-Vallabhacharya-ji-Maharaj-19826964486-100063566028142/&ved=2ahUKEwjQsqG38YOCAxU6zTgGHbGvAgUQFnoECBIQAQ&usg=AOvVaw2rI4wT3O39DB7_vO9uUmQs" key="facebook-link">
+            Facebook
+          </Link>
+        </h1>
+        <h1 style={{textDecorationColor:"red"}}>
+          <Link to="https://www.youtube.com/@jgadgurushrivallbhacharyaj5603" key="youtube-link">
+            YouTube
+          </Link>
+        </h1>
+      </div>
+    );
+  }
 }
